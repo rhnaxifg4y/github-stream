339ef810da2dda494544182fc42621b8f73a3f58
rhnaxifg4y <rhnaxifg4y@mouafik.fr>
2024-07-07 19:18:22 +0100

    Iterate

---
+++ clean.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import path from 'path';
+import fs from 'fs';
+
+var githubKeyForComments = ''; // public repositories scope
+
+const dataFilePath = path.join('./globe/bin/webgl-globe/data/data.json');
+
+// Read existing data from data.json
+let existingData = [];
+existingData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
+existingData.reverse();
+
+(async () => {
+    for (let i = 0; i < existingData.length; i++) {
+        console.log(`${i}/${existingData.length}`)
+        if (existingData[i].dce) {
+            try {
+                const deleteCommentEndpoint = existingData[i].dce
+                await axios.delete(deleteCommentEndpoint, {
+                    headers: {
+                        "Accept": "application/vnd.github+json",
+                        'Authorization': `Bearer ${githubKeyForComments}`,
+                        "X-GitHub-Api-Version": "2022-11-28"
+                    }
+                })
+            } catch (e) {
+                if (e.response.status != 404) {
+                    console.log(e.response)
+                    process.exit()
+                }
+            }
+        }
+    }
+})()
